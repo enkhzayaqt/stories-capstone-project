d8ac7354c279f0717dd0d730e6b8dcfe56360427
enkhzayaqt <jini.zaya@gmail.com>
2023-02-27 23:30:29 -0600

    clap added

--- frontend/src/components/Story/index.js
+++ frontend/src/components/Story/index.js
@@ -1,19 +1,28 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const Story = (props) => {
+    const dispatch = useDispatch();
     const { id, userId, title, body, image } = props.data;
-    const user = useSelector(state => state.stories.storyDetails.user)
+    // const user = useSelector(state => state.stories.storyDetails.user)
+    const [clap, setClap] = useState(0);
 
+    const handleClap = () => {
+        setClap(clap + 1);
+        dispatch()
+    }
     return (
         <div className="story-container">
             <div className="story-thumb-container">
-                <div className="story-thumb-author">User info {user.name}</div>
+                <div className="story-thumb-author">User info {userId}</div>
                 <a href={`/stories/${id}`} className="story-thumb-link">
                     <div className="story-thumb-title">{title}</div>
                 </a>
                 <div className="story-thumb-body">{body}</div>
-                <div className="story-thumb-meta">Writing</div>
+                <div className="story-thumb-meta" onClick={handleClap}>
+                    <i className="fa-solid fa-hands-clapping"></i>
+                    { clap}
+                </div>
             </div>
             <a href={`/stories/${id}`} className="story-thumb-link">
                 <div className="story-thumb-img">
