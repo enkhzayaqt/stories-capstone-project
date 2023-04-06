e666bf9c0e105328444b68cf08f25cabf6b4343a
enkhzayaqt <jini.zaya@gmail.com>
2023-04-05 23:13:37 -0500

    tags added to db

---
+++ backend/db/migrations/20230324155148-create-story-tag.js
@@ -0,0 +1,45 @@
+'use strict';
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('StoryTags', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      storyId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'Stories' },
+        onDelete:'CASCADE'
+      },
+      tagId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'Tags' },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    }, options);
+  },
+  async down(queryInterface, Sequelize) {
+    options.tableName = 'StoryTags'
+    await queryInterface.dropTable(options);
+  }
+};
